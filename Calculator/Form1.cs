6ec7972d1a19d5a43dd7f017624eeab5d3848cce
johnsonchou3 <johnsonchou3@gmail.com>
2021-07-27 09:32:20 +0800

    removed classes
    removed classes and made it form1-oriented

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json;
 
 namespace Calculator
 {
@@ -21,30 +25,40 @@ namespace Calculator
         public Form1()
         {
             InitializeComponent();
-            this.btnexe.Tag = new Execute();
-            this.btnadd.Tag = new Operation(this.btnadd.Text);
-            this.btnsub.Tag = new Operation(this.btnsub.Text);
-            this.btnmul.Tag = new Operation(this.btnmul.Text);
-            this.btndiv.Tag = new Operation(this.btndiv.Text);
-            this.btn0.Tag = new NumPad(this.btn0.Text);
-            this.btn1.Tag = new NumPad(this.btn1.Text);
-            this.btn2.Tag = new NumPad(this.btn2.Text);
-            this.btn3.Tag = new NumPad(this.btn3.Text);
-            this.btn4.Tag = new NumPad(this.btn4.Text);
-            this.btn5.Tag = new NumPad(this.btn5.Text);
-            this.btn6.Tag = new NumPad(this.btn6.Text);
-            this.btn7.Tag = new NumPad(this.btn7.Text);
-            this.btn8.Tag = new NumPad(this.btn8.Text);
-            this.btn9.Tag = new NumPad(this.btn9.Text);
-            this.btndec.Tag = new Dec();
-            this.btnce.Tag = new ClearEntry();
-            this.btnc.Tag = new ClearAll();
-            this.btnback.Tag = new Backspace();
-            this.btnPN.Tag = new PosNeg();
-            this.btnroot.Tag = new Root();
-            this.btnbrack1.Tag = new BracketOp(this.btnbrack1.Text);
-            this.btnbrack2.Tag = new BracketClose(this.btnbrack2.Text);
+            this.btnexe.Tag = "execute";
+            this.btnadd.Tag = "Operation?btnop=" + System.Web.HttpUtility.UrlEncode(this.btnadd.Text);
+            this.btnsub.Tag = "Operation?btnop=" + System.Web.HttpUtility.UrlEncode(this.btnsub.Text);
+            this.btnmul.Tag = "Operation?btnop=" + System.Web.HttpUtility.UrlEncode(this.btnmul.Text);
+            this.btndiv.Tag = "Operation?btnop=" + System.Web.HttpUtility.UrlEncode(this.btndiv.Text);
+            this.btn0.Tag = "Numpad?btnnum=" + this.btn0.Text;
+            this.btn1.Tag = "Numpad?btnnum=" + this.btn1.Text;
+            this.btn2.Tag = "Numpad?btnnum=" + this.btn2.Text;
+            this.btn3.Tag = "Numpad?btnnum=" + this.btn3.Text;
+            this.btn4.Tag = "Numpad?btnnum=" + this.btn4.Text;
+            this.btn5.Tag = "Numpad?btnnum=" + this.btn5.Text;
+            this.btn6.Tag = "Numpad?btnnum=" + this.btn6.Text;
+            this.btn7.Tag = "Numpad?btnnum=" + this.btn7.Text;
+            this.btn8.Tag = "Numpad?btnnum=" + this.btn8.Text;
+            this.btn9.Tag = "Numpad?btnnum=" + this.btn9.Text;
+            this.btndec.Tag = "Dec";
+            this.btnce.Tag = "ClearEntry";
+            this.btnc.Tag = "ClearAll";
+            this.btnback.Tag = "Backspace";
+            this.btnPN.Tag = "PosNeg";
+            this.btnroot.Tag = "Root";
+            this.btnbrack1.Tag = "BracketOp?bracket=" + System.Web.HttpUtility.UrlEncode(this.btnbrack1.Text);
+            this.btnbrack2.Tag = "BracketOp?bracket=" + System.Web.HttpUtility.UrlEncode(this.btnbrack2.Text);
         }
+        /// <summary>
+        /// 儲存winform 的CookieID以作辨識
+        /// </summary>
+        public static string CookieID { get; set; } = null;
+
+        /// <summary>
+        /// 整個winform 共用的caldata, 每次按按鍵都會給form1 作展示
+        /// </summary>
+        public static CalData WinformCaldata { get; set; }
+
 
         /// <summary>
         /// 按鍵的event handler, 會呼叫tag並使用其btnfunction
@@ -53,18 +67,30 @@ namespace Calculator
         /// <param name="e">事件數據</param>
         private void Button_Click(object sender, EventArgs e)
         {                 
-            Button b = (Button)sender;
-            if (b.Name != "label1")
-            {             
-                Btns abc = (Btns)b.Tag;
-                abc.BtnFunction();
-                ResultBox.Text = Btns.WinformCaldata.TempInputString;
-                CurOp.Text = Btns.WinformCaldata.DisplayOperation;
-                PreOrd.Text = Btns.WinformCaldata.DisplayOperation;
-                PreOrd.Text = Btns.WinformCaldata.Preordstring;
-                InOrd.Text = Btns.WinformCaldata.Inordstring;
-                PostOrd.Text = Btns.WinformCaldata.Postordstring;
+            Button btn = (Button)sender;
+            if (btn.Name != "label1")
+            {
+                WinformCaldata = PostRequest((string)btn.Tag);
+                ResultBox.Text = WinformCaldata.TempInputString;
+                CurOp.Text = WinformCaldata.DisplayOperation;
+                PreOrd.Text = WinformCaldata.DisplayOperation;
+                PreOrd.Text = WinformCaldata.Preordstring;
+                InOrd.Text = WinformCaldata.Inordstring;
+                PostOrd.Text = WinformCaldata.Postordstring;
             }
-        }                                    
+        }
+        private CalData PostRequest(string btntag)
+        {
+            string url = "https://localhost:44375/api/Math/";
+            url += btntag;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Headers["Cookie"] = CookieID;
+            var response = (HttpWebResponse)request.GetResponse();
+            CookieID = response.Headers["set-cookie"];
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            CalData caldata = Newtonsoft.Json.JsonConvert.DeserializeObject<CalData>(responseString);
+            return caldata;
+        }
     }
 }
