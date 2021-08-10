afba5e1e80e9a94fd38567ea3d53414e97ec475e
johnsonchou3 <johnsonchou3@gmail.com>
2021-08-10 10:52:26 +0800

    use async await
    replace thread with async await

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -6,6 +6,7 @@ using System.Web;
 using Newtonsoft.Json;
 using System.Threading;
 using System.Configuration;
+using System.Threading.Tasks;
 
 namespace Calculator
 {
@@ -20,30 +21,30 @@ namespace Calculator
         public Form1()
         {
             InitializeComponent();
-            this.btnexe.Tag = "?method=Execute";
-            this.btnadd.Tag = "?method=AddSubtract&button=" + System.Web.HttpUtility.UrlEncode(this.btnadd.Text);
-            this.btnsub.Tag = "?method=AddSubtract&button=" + System.Web.HttpUtility.UrlEncode(this.btnsub.Text);
-            this.btnmul.Tag = "?method=MultiplyDivision&button=" + System.Web.HttpUtility.UrlEncode(this.btnmul.Text);
-            this.btndiv.Tag = "?method=MultiplyDivision&button=" + System.Web.HttpUtility.UrlEncode(this.btndiv.Text);
-            this.btn0.Tag = "?method=NumPad&button=" + this.btn0.Text;
-            this.btn2.Tag = "?method=NumPad&button=" + this.btn2.Text;
-            this.btn1.Tag = "?method=NumPad&button=" + this.btn1.Text;
-            this.btn3.Tag = "?method=NumPad&button=" + this.btn3.Text;
-            this.btn4.Tag = "?method=NumPad&button=" + this.btn4.Text;
-            this.btn5.Tag = "?method=NumPad&button=" + this.btn5.Text;
-            this.btn6.Tag = "?method=NumPad&button=" + this.btn6.Text;
-            this.btn7.Tag = "?method=NumPad&button=" + this.btn7.Text;
-            this.btn8.Tag = "?method=NumPad&button=" + this.btn8.Text;
-            this.btn9.Tag = "?method=NumPad&button=" + this.btn9.Text;
-            this.btndec.Tag = "?method=AddDecimal";
-            this.btnce.Tag = "?method=ClearEntry";
-            this.btnc.Tag = "?method=ClearAll";
-            this.btnback.Tag = "?method=Backspace";
-            this.btnPN.Tag = "?method=PositiveNegative";
-            this.btnroot.Tag = "?method=Root";
-            this.btnbrack1.Tag = "?method=BracketOpen&button=" + HttpUtility.UrlEncode(this.btnbrack1.Text);
-            this.btnbrack2.Tag = "?method=BracketClose&button=" + HttpUtility.UrlEncode(this.btnbrack2.Text);
-            this.btninv.Tag = "?method=Invert";
+            this.buttonExecute.Tag = "?method=Execute";
+            this.buttonAdd.Tag = "?method=AddSubtract&button=" + System.Web.HttpUtility.UrlEncode(this.buttonAdd.Text);
+            this.buttonSubtract.Tag = "?method=AddSubtract&button=" + System.Web.HttpUtility.UrlEncode(this.buttonSubtract.Text);
+            this.buttonMultiply.Tag = "?method=MultiplyDivision&button=" + System.Web.HttpUtility.UrlEncode(this.buttonMultiply.Text);
+            this.buttonDivision.Tag = "?method=MultiplyDivision&button=" + System.Web.HttpUtility.UrlEncode(this.buttonDivision.Text);
+            this.button0.Tag = "?method=NumPad&button=" + this.button0.Text;
+            this.button2.Tag = "?method=NumPad&button=" + this.button2.Text;
+            this.button1.Tag = "?method=NumPad&button=" + this.button1.Text;
+            this.button3.Tag = "?method=NumPad&button=" + this.button3.Text;
+            this.button4.Tag = "?method=NumPad&button=" + this.button4.Text;
+            this.button5.Tag = "?method=NumPad&button=" + this.button5.Text;
+            this.button6.Tag = "?method=NumPad&button=" + this.button6.Text;
+            this.button7.Tag = "?method=NumPad&button=" + this.button7.Text;
+            this.button8.Tag = "?method=NumPad&button=" + this.button8.Text;
+            this.button9.Tag = "?method=NumPad&button=" + this.button9.Text;
+            this.buttonDecimal.Tag = "?method=AddDecimal";
+            this.buttonCE.Tag = "?method=ClearEntry";
+            this.buttonC.Tag = "?method=ClearAll";
+            this.buttonBack.Tag = "?method=Backspace";
+            this.buttonPositiveNegative.Tag = "?method=PositiveNegative";
+            this.buttonRoot.Tag = "?method=Root";
+            this.buttonBracketOpen.Tag = "?method=BracketOpen&button=" + HttpUtility.UrlEncode(this.buttonBracketOpen.Text);
+            this.buttonBracketClose.Tag = "?method=BracketClose&button=" + HttpUtility.UrlEncode(this.buttonBracketClose.Text);
+            this.buttonInvert.Tag = "?method=Invert";
         }
 
         /// <summary>
@@ -66,71 +67,57 @@ namespace Calculator
         /// </summary>
         /// <param name="sender">觸發者</param>
         /// <param name="e">事件數據</param>
-        private void Button_Click(object sender, EventArgs e)
-        {                 
+        private async void Button_Click(object sender, EventArgs e)
+        {
             Button btn = (Button)sender;
-            StatusLab.Text = "Processing...";
-            if (btn.Name != "label1")
+            StatusLabel.Text = "Processing...";
+            try
             {
-                Thread thread = new Thread(SetValue);
-                thread.Start();
-                void SetValue()
+                //成功回傳200
+                WinformCaldata = await PostRequestAsync((string)btn.Tag);
+                ResultBox.Text = WinformCaldata.TempInputString;
+                CurrentOperation.Text = WinformCaldata.DisplayOperation;
+                PreOrderLabel.Text = WinformCaldata.DisplayOperation;
+                PreOrderLabel.Text = WinformCaldata.Preordstring;
+                InOrderLabel.Text = WinformCaldata.Inordstring;
+                PostOrderLabel.Text = WinformCaldata.Postordstring;
+            }
+            catch (WebException exception)
+            {
+                try
                 {
-                    PostRequest((string)btn.Tag);
+                    //有response 但非200
+                    StreamReader streamreader = new StreamReader(exception.Response?.GetResponseStream());
+
+                    StatusLabel.Text = "Status Code: " + (int)((HttpWebResponse)exception.Response).StatusCode + "-" + streamreader.ReadToEnd();
+
+                }
+                catch (ArgumentNullException)
+                {
+                    //沒有response
+
+                    StatusLabel.Text = "WebAPI is not available currently.";
+
                 }
             }
         }
-    
+
         /// <summary>
         /// 送出post 請求的function
         /// </summary>
         /// <param name="btntag">需傳入btntag到url</param>
-        private void PostRequest(string btntag)
+        private async Task<CalData> PostRequestAsync(string btntag)
         {
             string tempurl = url + btntag;
             var request = (HttpWebRequest)WebRequest.Create(tempurl);
             request.Method = "POST";
             request.Headers["Cookie"] = CookieID;
-
-            try
-            {
-                //成功回傳200
-                var response = (HttpWebResponse)request?.GetResponse();
-                   CookieID = response.Headers["set-cookie"];
-                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                CalData caldata = JsonConvert.DeserializeObject<CalData>(responseString);
-                WinformCaldata = caldata;
-                this.Invoke((MethodInvoker)delegate
-                {
-                    StatusLab.Text = "Status Code: " + (int)response.StatusCode;
-                    ResultBox.Text = WinformCaldata.TempInputString;
-                    CurOp.Text = WinformCaldata.DisplayOperation;
-                    PreOrd.Text = WinformCaldata.DisplayOperation;
-                    PreOrd.Text = WinformCaldata.Preordstring;
-                    InOrd.Text = WinformCaldata.Inordstring;
-                    PostOrd.Text = WinformCaldata.Postordstring;
-                });
-            }
-            catch (WebException ex)
-            {
-                try
-                {
-                    //有response 但非200
-                    StreamReader streamreader = new StreamReader(ex.Response?.GetResponseStream());
-                    this.Invoke((MethodInvoker)delegate
-                    {
-                        StatusLab.Text = "Status Code: " + (int)((HttpWebResponse)ex.Response).StatusCode + "-" + streamreader.ReadToEnd();
-                    });
-                }
-                catch (ArgumentNullException)
-                {
-                    //沒有response
-                    this.Invoke((MethodInvoker)delegate
-                    {
-                        StatusLab.Text = "WebAPI is not available currently.";
-                    });
-                }
-            }
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            CookieID = response.Headers["set-cookie"];
+            StatusLabel.Text = "Status Code: " + (int)response.StatusCode;
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            CalData caldata = JsonConvert.DeserializeObject<CalData>(responseString);
+            return caldata;
         }
     }
 }
